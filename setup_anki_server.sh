64143077271a42d65d99cd9a4c81ee22c1fca7d8
fd3kyt <fd3kyt@gmail.com>
2018-09-30 15:00:03 +0800

    update

--- setup_anki_server.sh
+++ setup_anki_server.sh
@@ -7,9 +7,11 @@ set -e
 export LC_ALL=C  # if "unsupported locale setting"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python2.7 get-pip.py --force-reinstall
+
 pip install --upgrade pip
 pip install --upgrade httplib2
-apt install python-pastescript
+pip install webob PasteDeploy PasteScript sqlalchemy simplejson
+# apt install python-pastescript
 
 apt-get install supervisor
 cp ./supervisor/anki_server.conf /etc/supervisor/conf.d/anki_server.conf
