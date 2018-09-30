4dc1bdebd848c5bd5b3f93b7624a7bc6f99ee502
fd3kyt <fd3kyt@gmail.com>
2018-09-30 18:01:44 +0800

    rename main scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo ./install_dependence.sh
-sudo ./new_user.sh
-su anki_server -c ./add_account.sh
+sudo ./add_user_anki_server.sh
+su anki_server -c ./add_anki_account.sh
 sudo ./start_anki_server.sh
