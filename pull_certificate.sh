ca73abd8f1c95f4006069aceff1dbea7c54005c4
fd3kyt <fd3kyt@gmail.com>
2018-09-30 18:18:27 +0800

    update pull_certificate.sh

--- pull_certificate.sh
+++ pull_certificate.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 target_host=$1
+output_dir=$2
 
 if [[ -z ${target_host} ]];then
-    echo "Usage: ${0} username@host"
+    echo "Usage: ${0} username@host [output_dir]"
     exit 1
 fi
 
-scp "${target_host}:/home/anki_server/anki_server/nginx/certificate/CA.crt" \
-    ./nginx/certificate/
+if [[ -z $output_dir ]];then
+    output_dir="."
+fi
+
+scp "${target_host}:/home/anki_server/server_data/nginx_certificate/CA.crt" "$output_dir"
