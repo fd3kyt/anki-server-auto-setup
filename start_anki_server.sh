7dd61f0815a37d90dc3af96e2e0dceb8acebbd7d
fd3kyt <fd3kyt@gmail.com>
2020-08-09 14:32:14 +0800

    add ankiweb_proxy

--- start_anki_server.sh
+++ start_anki_server.sh
@@ -4,5 +4,5 @@ echo "########## start supervisor and nginx ##########"
 
 ./supervisor_template.sh > /etc/supervisor/conf.d/anki_server.conf
 supervisorctl reload
-cp ./nginx/anki_server /etc/nginx/sites-enabled/
+cp ./nginx/* /etc/nginx/sites-enabled/
 nginx -s reload
